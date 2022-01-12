7aebb79053e200bda704d7a3e03ab066c9fd0e1d
Alejandro A <ale@doges.dev>
2022-01-12 03:11:58 -0500

    added game over: walls and self destruct. Added max score.

--- snake/game.py
+++ snake/game.py
@@ -36,6 +36,9 @@ RED = (255, 0, 0)
 DARKRED = (150, 0, 0)
 WHITE = (255, 255, 255)
 
+LIGHT = (199, 240, 216)
+DARK = (67, 82, 61)
+
 # Set Fonts
 font = pygame.font.SysFont('gabriola', 48)
 
@@ -48,13 +51,20 @@ score_text = font.render(f"Score: {score}", True, GREEN, DARKRED)
 score_rect = score_text.get_rect()
 score_rect.topleft = (10,10)
 
+max_score_text = font.render(f"Max: {max_score}", True, DARKGREEN, WHITE)
+max_score_rect = max_score_text.get_rect()
+max_width = max_score_rect.width
+max_score_rect.topright = (WINDOW_WIDTH - 30, 10)
+print(max_score_rect)
+
+
 game_over_text = font.render("GAME OVER", True, RED, DARKGREEN)
 game_over_rect = game_over_text.get_rect()
 game_over_rect.center = (WINDOW_WIDTH//2, WINDOW_HEIGHT//2)
 
-continue_text = font.render("Continue? Press any key to play a new game", True, RED, DARKGREEN)
+continue_text = font.render("Press any key to play again", True, RED, DARKGREEN)
 continue_rect = continue_text.get_rect()
-continue_rect.center = (WINDOW_WIDTH//2, WINDOW_HEIGHT + 60)
+continue_rect.center = (WINDOW_WIDTH//2, WINDOW_HEIGHT//2 + 100)
 
 # Set Sounds and Music
 pick_up_sound = pygame.mixer.Sound(os.path.join("sounds", "5_snake_pick_up_sound.wav"))
@@ -66,7 +76,7 @@ head_coord = (head_x, head_y, SNAKE_SIZE, SNAKE_SIZE)
 body_coords = []
 
 apple_rect = pygame.draw.rect(display_surface, RED, apple_coord)
-head_rect = pygame.draw.rect(display_surface, GREEN, head_coord)
+head_rect = pygame.draw.rect(display_surface, DARK, head_coord)
 #pygame.image.load()
 
 # Main game loop
@@ -90,40 +100,74 @@ while running:
             if event.key == pygame.K_DOWN:
                 snake_dx = 0
                 snake_dy = 1 * SNAKE_SIZE
-            print(event.key)
-            print(snake_dx, snake_dy)
-            print(head_x, head_y)
+            #print(event.key)
+            #print(snake_dx, snake_dy)
+            #print(head_x, head_y)
 
-    # update position of x, y
+    # add the head coor to the 1st index of the body coor list
+    # move all of the snakes body by one pos in the list
+    body_coords.insert(0, head_coord)
+    body_coords.pop()
+    
+    # update position of x, y of the snake head and makes a new coordinate
     head_x += snake_dx
     head_y += snake_dy
     head_coord = (head_x, head_y, SNAKE_SIZE, SNAKE_SIZE)
+
+    # Check for GAME OVER
+    if head_rect.left < 0 or head_rect.right > WINDOW_HEIGHT or head_rect.top < 0 or head_rect.bottom > WINDOW_HEIGHT or head_coord in body_coords:
+        print("game over condition")
+        display_surface.blit(game_over_text, game_over_rect)
+        display_surface.blit(continue_text, continue_rect)
+        if score > max_score:
+            max_score = score
+        pygame.display.update()
+        # pause
+        is_paused = True
+        while is_paused:
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    score = 0
+                    head_x = WINDOW_WIDTH //2
+                    head_y = WINDOW_HEIGHT // 2 + 100
+                    head_coord = (head_x, head_y, SNAKE_SIZE, SNAKE_SIZE)
+                    body_coords = []
+                    snake_dx = 0
+                    snake_dy = 0
+                    is_paused = False
+                if event.type == pygame.QUIT:
+                    is_paused = False
+                    running = False
+
 
     # check collisions
     if head_rect.colliderect(apple_rect):
         score += 1
         pick_up_sound.play()
-        print("collision")
+        #print("collision")
 
         apple_x = random.randint(0, WINDOW_WIDTH - SNAKE_SIZE)
         apple_y = random.randint(0, WINDOW_HEIGHT - SNAKE_SIZE)
         apple_coord = (apple_x, apple_y, SNAKE_SIZE, SNAKE_SIZE)
+        body_coords.append(head_coord)
         title_text = font.render("", True, WHITE, WHITE)
     
     # Update HUD
     score_text = font.render(f"Score: {score}", True, GREEN, DARKRED)
-    
-
+    max_score_text = font.render(f"Max: {max_score}", True, DARKGREEN, WHITE)
 
     # fill surface
     display_surface.fill(WHITE)
     # HUD
     display_surface.blit(title_text, title_rect)
     display_surface.blit(score_text, score_rect)
+    display_surface.blit(max_score_text, max_score_rect)
 
     # Blit assets
     head_rect = pygame.draw.rect(display_surface, GREEN, head_coord)
     apple_rect = pygame.draw.rect(display_surface, RED, apple_coord)
+    for body in body_coords:
+        pygame.draw.rect(display_surface, DARKGREEN, body)
 
     # Update display
     pygame.display.update()
