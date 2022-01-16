5c5d2b02e24555c38937f9df765c36126a99c259
Alejandro A <ale@doges.dev>
2022-01-16 16:36:05 -0500

    burger dog: initial values and texts set

--- burger_dog/game.py
+++ burger_dog/game.py
@@ -10,6 +10,68 @@ WINDOW_HEIGHT = 600
 display_surface = pygame.display.set_mode( (WINDOW_WIDTH, WINDOW_HEIGHT) )
 pygame.display.set_caption("Burger Dog")
 
+# Set FPS and Clock
+FPS = 60
+clock = pygame.time.Clock()
+
+# Set game Values
+PLAYER_STARTING_LIVES = 3
+PLAYER_NORMAL_VELOCITY = 5
+PLAYER_BOOST_VELOCITY = 10
+STARTING_BOOST_LEVEL = 100
+STARTING_BURGER_VELOCITY = 3
+BURGER_ACCELERATION = .25
+
+score = 0
+burger_points = 0
+burgers_eaten = 0
+
+player_lives = PLAYER_STARTING_LIVES
+player_velocity = PLAYER_NORMAL_VELOCITY
+
+boost_level = STARTING_BOOST_LEVEL
+
+burger_velocity = STARTING_BURGER_VELOCITY
+
+
+# Set Colors
+ORANGE = (246, 170, 54)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+
+# Set Fonts
+font = pygame.font.Font("fonts/WashYourHand.ttf", 32)
+
+# Set Text
+points_text = font.render(f"Burger Points: {burger_points}", True, ORANGE)
+points_rect = points_text.get_rect()
+points_rect.topleft = (10, 10)
+
+score_text = font.render(f"Score: {score}", True, ORANGE)
+score_rect = score_text.get_rect()
+score_rect.topleft = (10, 50)
+
+title_text = font.render(f"Burger Dog", True, ORANGE)
+title_rect = title_text.get_rect()
+title_rect.centerx = WINDOW_WIDTH // 2
+title_rect.y = 10
+
+eaten_text = font.render(f"Burgers Eaten: {burgers_eaten}")
+eaten_rect = eaten_text.get_rect()
+eaten_rect.centerx = WINDOW_WIDTH // 2
+eaten_rect.y = 50
+
+lives_text = font.render(f"Lives: {player_lives}", True, ORANGE)
+lives_rect = lives_text.get_rect()
+lives_rect.topright = (WINDOW_WIDTH - 10, 10)
+
+boost_text = font.render(f"Boost: {boost_level}", True, ORANGE)
+boost_rect = boost_text.get_rect()
+boost_rect.top_right = (WINDOW_WIDTH -10, 50)
+
+# Set music
+
 # Main game loop
 running = True
 while running:
